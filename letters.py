9ec805a5dc108d0fc331dde699835194f3ef52ef
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-10 16:16:32 +0300

    fixed problems

--- letters.py
+++ letters.py
@@ -1,14 +1,14 @@
 def get_letters() -> tuple:
     """
-    :return: List of lowercase and list of uppercase letters.
+    :return: tuple with List of lowercase and list of uppercase letters.
     """
     return (create_list_letters('a', 'z'),
             create_list_letters('A', 'Z'))
 
 
-def create_list_letters(start: chr, end: chr) -> list:
+def create_list_letters(start: str, end: str) -> list:
     """
-    Makes range of int- using ord function between start to end.
+    Make a range of int- using ord function between start to end.
     Then, maps all range to chr and returns it.
     :param start: Start range.
     :param end: End range.
