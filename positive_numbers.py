5cf690c253f917aeab383b1260c7416c02c83578
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-10 16:01:45 +0300

    Split the io from logic

--- positive_numbers.py
+++ positive_numbers.py
@@ -1,18 +1,28 @@
 def read_numbers() -> map:
     """
-    Reads numbers from standard input.
+    Read numbers from standard input.
+    The input must be separated with ','. for example: 10,-7,6,5
     :return: Map with all numbers as int
     """
     return map(int, input().split(','))
 
 
-def positive_numbers() -> list:
+def positive_numbers(numbers: map) -> list:
     """
+    Filter all negative numbers.
+    :param numbers: map of numbers.
     :return: List with all values are positive
     """
-    numbers = read_numbers()
     return list(filter(lambda num: num >= 0, numbers))
 
 
+def main() -> None:
+    """
+    Read numbers from user and print only the positives.
+    """
+    numbers = read_numbers()
+    print(positive_numbers(numbers))
+
+
 if __name__ == '__main__':
-    print(positive_numbers())
+    main()
